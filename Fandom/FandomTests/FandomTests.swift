3a1d79fde585190b637509a28ba39d2e658faed0
nateperry <ndp4570@rit.edu>
2015-02-05 19:45:31 -0500

    removed old project, created with tab bar controller

--- Fandom/FandomTests/FandomTests.swift
+++ Fandom/FandomTests/FandomTests.swift
@@ -3,7 +3,7 @@
 //  FandomTests
 //
 //  Created by Student on 2/5/15.
-//  Copyright (c) 2015 NatePerry. All rights reserved.
+//  Copyright (c) 2015 Fandom. All rights reserved.
 //
 
 import UIKit
