7af685fc184d468fffc3c64620b62848ddd5084e
nateperry <ndp4570@rit.edu>
2015-02-06 23:29:54 -0500

    added cell formatting

--- Fandom/Fandom/Concert.swift
+++ Fandom/Fandom/Concert.swift
@@ -11,10 +11,16 @@ import CoreData
 
 class Concert: NSObject {
     
-    let bands = "", venue = ""
+    let bands = "", venue = "", date = "", score = 0
     
     init (dataSet:NSManagedObject) {
         self.bands = dataSet.valueForKey("bands") as String!
         self.venue = dataSet.valueForKey("venue") as String!
+        self.score = dataSet.valueForKey("score") as Int!
+        // format the date
+        var theDate = dataSet.valueForKey("date") as NSDate
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "MM/dd/yyyy" // superset of OP's format
+        self.date = dateFormatter.stringFromDate(NSDate())
     }
 }
