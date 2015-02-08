94d38474a1fb7fbb044666ca241487323f2aac08
kerpshocker <nate.kierpiec@gmail.com>
2015-02-07 19:38:10 -0500

    added new files

---
+++ Fandom/Fandom/AboutViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Fandom
+//
+//  Created by Student on 2/5/15.
+//  Copyright (c) 2015 Fandom. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
